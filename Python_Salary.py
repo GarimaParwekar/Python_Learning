5bf571719376d21dbfd145e3d2ff5dc9e7888298
GarimaParwekar <parwekar.garima@gmail.com>
2024-07-25 23:40:01 -0400

    Update Python_Salary.py

--- Python_Salary.py
+++ Python_Salary.py
@@ -1,4 +1,4 @@
 hours = int(input('How many hours did you work last month? '))
 rate = float(input ('What is your hourly rate? '))
 total = hours*rate
-print('Last month, you earned', total, 'dollars')
+print('Last month, you have earned', total, 'dollars')
